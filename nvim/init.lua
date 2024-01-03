68d210b8ae303cdaab3403294dc3ea8f0e2cf62e
Milvus01 <cotcos.tansin@gmail.com>
2024-01-03 18:41:28 +0100

    add LazyVim in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,65 @@
-require("plugins")
+-- install and load `lazy` plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+require("lazy").setup({
+    "Mofiqul/dracula.nvim",
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("config.nvim-tree")
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"rust-analyzer",
+				"clang-format",
+				"cpplint",
+				"cpptools",
+				"html-lsp",
+				"css-lsp",
+				"pylint",
+				"pyre",
+				"pyright",
+				"python-lsp-server",
+				"isort",
+				"pylizer",
+			},
+		},
+	},
+	--"williamboman/nvim-lsp-installer",
+    "neovim/nvim-lspconfig",
+    {
+        "simrat39/rust-tools.nvim",
+        ft = "rust",
+        dependencies = "neovim/nvim-lspconfig",
+    },
+    spec = {
+        { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+        { import = "lazyvim.plugins.extras.lang.rust" },
+    },
+})
+
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 vim.wo.number = true
 vim.wo.relativenumber = true
 
@@ -10,48 +68,24 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
-require('mason').setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
-})
-require('nvim-lsp-installer').setup()
-require('neoconf').setup({
-  -- name of the local settings files
-  local_settings = ".neoconf.json",
-  -- name of the global settings file in your Neovim config directory
-  global_settings = "neoconf.json",
-  -- send new configuration to lsp clients when changing json settings
-  live_reload = true,
-  -- set the filetype to jsonc for settings files, so you can use comments
-  -- make sure you have the jsonc treesitter parser installed!
-  filetype_jsonc = true,
-  plugins = {
-    -- configures lsp clients with settings in the following order:
-    -- - lua settings passed in lspconfig setup
-    -- - global json settings
-    -- - local json settings
-    lspconfig = {
-      enabled = true,
-    },
-    -- configures jsonls to get completion in .nvim.settings.json files
-    jsonls = {
-      enabled = true,
-      -- only show completion in json settings for configured lsp servers
-      configured_servers_only = true,
-    },
-    -- configures lua_ls to get completion of lspconfig server settings
-    lua_ls = {
-      -- by default, lua_ls annotations are only enabled in your neovim config directory
-      enabled_for_neovim_config = true,
-      -- explicitely enable adding annotations. Mostly relevant to put in your local .nvim.settings.json file
-      enabled = false,
-    },
-  },
-})
-require('lspconfig').rust_analyzer.setup{}
+local set = vim.opt
+set.tabstop = 2
+set.softtabstop = 2
+set.shiftwidth = 2
+set.showmode = true
+set.ignorecase = true
+set.smartcase = true
+set.expandtab = true
+--vim.cmd([[ colorscheme dracula ]])
+--require("mason").setup({
+--    ui = {
+--        icons = {
+--            package_installed = "✓",
+--            package_pending = "➜",
+--            package_uninstalled = "✗"
+--        }
+--    }
+--})
+--require("nvim-lsp-installer").setup()
+--require("lspconfig").rust_analyzer.setup{}
 
