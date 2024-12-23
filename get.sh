aaaf4fb8f0e165c26a414e77b5e5b91701e053b6
milvusAquila <milvus.aquila@ik.me>
2024-12-23 11:45:02 +0100

    update the get.sh file used for package tracking

--- get.sh
+++ get.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/bash
 
-pacman -Qqne > ne.txt
-pacman -Qqme > me.txt
-pacman -Qqnd > nd.txt
-pacman -Qqmd > md.txt
+# get list of package
+pacman -Qqne > ne.txt # native explicite
+pacman -Qqme > me.txt # AUR explicite
+pacman -Qqnd > nd.txt # native dependencies
+pacman -Qqmd > md.txt # AUR dependencies
 
+# get number of package
 pacman -Q | wc -l > nb
 
+# get flatpak list
+flatpak list --columns=application -a > fd.txt # dependencies and explicite
+flatpak list --columns=application --app > fe.txt # explicite
+
+# add flatpak number to nb
+cat fd.txt | wc -l >> nb
+
+# for flatpak: remove explicite from dependencies
+fe=$(cat $"fe.txt")
+for i in $fe; do
+  sed -i "/^${i}$/d" ./fd.txt
+done
+
+# add list of personal files
+echo "$(ls -A ~)" > home/home.txt
+echo "$(ls ~/.cache)" > home/cache.txt
+echo "$(ls ~/.config)" > home/config.txt
+echo "$(ls ~/.local/state)" > home/state.txt
+echo "$(ls ~/.local/share)" > home/share.txt
